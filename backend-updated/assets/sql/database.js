fe2abfefe83be8bf379af4e9bee6deff6d68a546
zachs1326 <zachs1326@gmail.com>
2026-04-13 16:45:40 -0400

    getting rid of personal sql password

--- backend-updated/assets/sql/database.js
+++ backend-updated/assets/sql/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const db = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "student123",
+    password: "",
     database: "glt_dashboard",
     dateStrings: ["DATETIME", "TIMESTAMP"],
 });
